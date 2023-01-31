df57a52935b3d904ddc12af0beae3dac551cd48f
Vitaly Vegera <surewek@gmail.com>
2023-01-31 18:28:55 +0300

    feat: add start server function

--- palletport-server/index.ts
+++ palletport-server/index.ts
@@ -1,1 +1,15 @@
 import express from 'express';
+
+const PORT = process.env.PORT ?? 5300;
+
+const app = express();
+
+const startServer = (): void => {
+    try {
+        app.listen(PORT, () => console.log('It work WOW!'));
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+startServer();
